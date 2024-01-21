8af7c3b570dee0559c6c4ab95ceb73a5d9700eb5
Hristina <hristinastankova89@gmail.com>
2024-01-21 17:28:06 +0100

    SimpleEvents manage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,4 +11,52 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'SimpleEvent';
+  message = "message par defaut";
+
+
+  handleHelloClick(){
+    alert ('Hello');
+  }
+
+  handleHelloDoubledClock(){
+    alert ('Hello Hello');
+  }
+
+  handleEvent (event: Event){
+    if(event.target instanceof HTMLButtonElement)
+    {
+      alert('ok')
+    }
+const button = event.target as HTMLButtonElement;
+console.log(button.value);
+  }
+
+  handleEventTwo (event: Event){
+    if(event.target instanceof HTMLHeadingElement)
+    {
+      alert('stop')
+    }
+    const header = event.target as HTMLHeadElement;
+    console.log(header.innerText);
+  }
+
+  handleMouseEvent(mousevent: MouseEvent){
+    console.log(`${mousevent.clientX} ${mousevent.clientY}`);
+
+  }
+
+  handleAndGetProperty(event: Event): string {
+    return (event.target as HTMLInputElement).value;
+  }
+  handleKeyDown(event: Event){
+    const ev = event as KeyboardEvent;
+    console.log(ev.key);
+  }
+  // //handleKeyDown(event: Event) {
+  // const ev = event as KeyboardEvent;
+  // if (ev.key === 'Enter') {
+  //   // Faire quelque chose lorsque la touche Entrée est pressée
+  // }
 }
+
+
